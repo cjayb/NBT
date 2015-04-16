9c0a7d3b43f3bdf28c79da553d81a35c2b768c4d
Simon-Shlomo Poil <poil.simonshlomo@nbt-analytics.com>
2015-04-16 18:14:35 +0200

    added nbt_injectSubjectInfo to update SubjectInfo based on the fileName

---
+++ Files/nbt_injectSubjectInfo.m
@@ -0,0 +1,20 @@
+function nbt_injectSubjectInfo(startpath)
+nbt_fileLooper(startpath, 'mat' , 'info', @innerfunc, 0)
+end
+
+function innerfunc(fileName)
+load(fileName, 'SubjectInfo')
+fidx = strfind(fileName,filesep);
+[projectID,leftover] = strtok(fileName(fidx(end)+1:end),'.');
+[subjectID,leftover] = strtok(leftover,'.');
+subjectID = str2double(subjectID(2:end));
+[dateRecord,leftover] = strtok(leftover,'.');
+[conditionID] = strtok(leftover,'.');
+conditionID = strtok(conditionID,'_');
+SubjectInfo.projectInfo = [ projectID '.mat'];
+SubjectInfo.subjectID =  subjectID;
+SubjectInfo.conditionID =  conditionID;
+SubjectInfo.lastUpdate = datestr(now);
+SubjectInfo.fileName =  fileName(fidx(end):end);
+save(fileName,'SubjectInfo','-append');
+end
