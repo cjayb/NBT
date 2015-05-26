92c576230060d8eac826ea92c33ac3f870a5f7b4
ssimpraga <lyra222@gmail.com>
2015-05-26 12:42:46 +0200

    small Single conversion fix and split groups for compare biomarkers

--- supportfunctions/nbt_convertToSingle.m
+++ supportfunctions/nbt_convertToSingle.m
@@ -1,6 +1,12 @@
 function Data=nbt_convertToSingle(Data)
 if(iscell(Data))
-    Data = cellfun(@single,Data,'UniformOutput',false);
+    if ~iscell(Data{1})
+        Data = cellfun(@single,Data,'UniformOutput',false);
+    else
+       for j=1:length(Data)
+         Data{j} = cellfun(@single,Data{j},'UniformOutput',false);  
+       end
+    end
 else
     Data = single(Data);
 end
