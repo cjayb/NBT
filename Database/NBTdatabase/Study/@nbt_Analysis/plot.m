5d7292491c7c1e269d2a6ae9b06f1d516723b6b7
ssimpraga <lyra222@gmail.com>
2015-06-29 18:05:41 +0200

    minor

--- Database/NBTdatabase/Study/@nbt_Analysis/plot.m
+++ Database/NBTdatabase/Study/@nbt_Analysis/plot.m
@@ -3,13 +3,18 @@ function plot(AnalysisObj)
 disp('break')
 %First we sort into the different classes:
 % QBiomarkers
-QBidx = nbt_searchvector(AnalysisObj.data{1}.classes,{'nbt_QBiomarker'});
+
+for j=1:size(AnalysisObj.data,2)
+    if ~isempty(AnalysisObj.data{j}) non_empty_ind = j; end
+end
+
+QBidx = nbt_searchvector(AnalysisObj.data{non_empty_ind}.classes,{'nbt_QBiomarker'});
 if(~isempty(QBidx))
    nbt_plotQbiomarkerTable(AnalysisObj, QBidx) 
 end
 
 % SignalBiomarkers
-SBidx = nbt_searchvector(AnalysisObj.data{1}.classes,{'nbt_SignalBiomarker'});
+SBidx = nbt_searchvector(AnalysisObj.data{non_empty_ind}.classes,{'nbt_SignalBiomarker'});
 
 
 
